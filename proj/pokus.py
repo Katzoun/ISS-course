5249a9312af677ac26e5e39f857c160eade4d4e9
Katzoun <tomas.janousek02@gmail.com>
2025-11-17 15:44:39 +0100

    proj finished protokol init

--- proj/pokus.py
+++ proj/pokus.py
@@ -1,6 +1,5 @@
 # needed
 import os
-import pickle
 import re
 import soundfile as sf
 from IPython.display import Audio
@@ -9,10 +8,10 @@ import numpy as np
 # recommended ...
 from scipy import signal
 from scipy.io import wavfile
-from scipy.signal import spectrogram, ShortTimeFFT
+from scipy.signal import spectrogram
 from scipy.ndimage import maximum_filter
 from collections import defaultdict
-
+import pickle
 from scipy.fft import fft, ifft, fftfreq
 import scipy.io
 import matplotlib.pyplot as plt
@@ -21,7 +20,6 @@ from typing import Optional
 import time
 from tqdm import tqdm
 
-print(os.getcwd())
 # SETTINGS
 SAMPLE_RATE = 16000
 NFFT = 2048
@@ -30,23 +28,26 @@ FRAME_DURATION = 0.1  # in seconds
 NPERFRAME = int(SAMPLE_RATE * FRAME_DURATION)
 NOVERLAP = NPERFRAME // 8
 
-
 # parameters for peak extraction
 NEIGHBORHOOD_SIZE = (25, 25)  # size of the neighborhood to consider for max filtering
 PEAKS_TO_KEEP = 256  # maximum number of peaks to keep for each spectrogram
 
 # fingerprinting parameters
 TARGET_ZONE_OFFSET = 0.1  # seconds
-TARGET_ZONE_DURATION = 1.8  # seconds
-TARGET_ZONE_FREQUENCY_BAND = 4000  # Hz
+TARGET_ZONE_DURATION = 1.8 # seconds
+TARGET_ZONE_FREQUENCY_BAND = 4500  # Hz
 
 # parameters for fingerprint storage
-USE_FILE = True # if True, use file-based storage of fingerprints, 
-                # else use in-memory dictionary that has to be rebuilt each time
+LOAD_FROM_FILE = False  # if True, load existing file-based storage of fingerprints, 
+                        # else use in-memory dictionary that has to be rebuilt each time        
+SAVE_TO_FILE = True  # if True, save fingerprint database to file after building
+
 FILE_NAME = "fingerprint.pkl"
 
 
 # print parameters
+print("==============PARAMETERS============")
+print(f"CWD = {os.getcwd()}" )
 print(f"SAMPLE RATE = {SAMPLE_RATE}" )
 print(f"NFFT = {NFFT}" )
 print(f"FRAME DURATION (s) = {FRAME_DURATION}" )
@@ -54,7 +55,10 @@ print(f"SAMPLES PER FRAME = {NPERFRAME}" )
 print(f"OVERLAP (SAMPLES) = {NOVERLAP}" )
 print(f"NEIGHBORHOOD SIZE = {NEIGHBORHOOD_SIZE}" )
 print(f"PEAKS TO KEEP = {PEAKS_TO_KEEP}" )
-print(f"USE FILE-BASED STORAGE = {USE_FILE}" )
+print("===============STORAGE=============")
+print(f"LOAD FROM FILE = {LOAD_FROM_FILE}" )
+print(f"SAVE TO FILE = {SAVE_TO_FILE}" )
+print(f"FILE NAME = {FILE_NAME}" )
 
 
 
@@ -215,15 +219,17 @@ def generate_fingerprint(signal, song_index: Optional[int] = None):
     _, peaks = extract_peaks(f, t, Sxx)
     return generate_hashes(peaks, song_index=song_index)
 
-def build_fingerprint_database(known_signals, use_file: Optional[bool] = False, filename: Optional[str] ="fingerprint.pkl"):
+def build_fingerprint_database(known_signals, load_from_file: Optional[bool] = False, save_to_file: Optional[bool] = True, filename: Optional[str] ="fingerprint.pkl"):
     """Build hash database from all known songs."""
     
     fingerprint_db = defaultdict(list)  # {hash_value: [(time_offset, song_index)]}
     
-    if use_file:
+    # if file-based storage is used, try to load existing database, else build new one
+    if load_from_file:
         if os.path.exists(filename):
             print("Loading fingerprint database from file...")
             fingerprint_db = load_fingerprint_db_pickle(filename)
+            print(f"Total unique hashes: {len(fingerprint_db)}")
             return fingerprint_db
         else:
             print("Fingerprint database file not found. Building new database...")
@@ -241,7 +247,9 @@ def build_fingerprint_database(known_signals, use_file: Optional[bool] = False, 
     end_time = time.time()
     print(f"\nDatabase built in {end_time - start_time:.2f} seconds")
     print(f"Total unique hashes: {len(fingerprint_db)}")
-    if use_file:
+    
+    # save database to file
+    if save_to_file:
         save_fingerprint_db_pickle(fingerprint_db, filename=filename)
 
     return fingerprint_db
@@ -260,52 +268,88 @@ def load_fingerprint_db_pickle(filename: str ="fingerprint.pkl"):
     print(f"Total unique hashes: {len(fingerprint_db)}")
     return fingerprint_db
 
-def recognize_signal(signal, fingerprint_db):
+def recognize_signal(input_signal, fingerprint_db):
     """Recognize the song from the given signal using the fingerprint database."""
-    hashes = generate_fingerprint(signal)
+    query_hashes = generate_fingerprint(input_signal)
     matches = defaultdict(int)  # {song_index: count_of_matching_hashes}
 
-    for hash_value, time_offset in hashes:
+    # for each hash in the signal, check if it exists in the database
+    for hash_value, _ in query_hashes:
         if hash_value in fingerprint_db:
             entries = fingerprint_db[hash_value]
-            for entry_time_offset, song_index in entries:
+            for _, song_index in entries:
                 matches[song_index] += 1
     
     return matches
-    # if matches:
-    #     recognized_song = max(matches, key=matches.get)
-    #     print("Top 5 matches:")
-    #     sorted_matches = sorted(matches.items(), key=lambda item: item[1], reverse=True)
-    #     print("Top matches:")
-    #     for song_index, match_count in sorted_matches[:5]:
-    #         print(f"Song index: {song_index}, Matching hashes: {match_count}")
 
-    #     print(f"Recognized song index: {recognized_song} with {matches[recognized_song]} matching hashes")
-    #     return recognized_song
-    # else:
-    #     print("No matching song found.")
-    #     return None
+def recognize_signal_time_offset(input_signal, fingerprint_db):
+    """Recognize the song from the given signal using the fingerprint database with time offset alignment."""
+    query_hashes = generate_fingerprint(input_signal)
     
-def compute_similarity_matrix(valid_signals, fingerprint_db , N_valid: int, N_known: int):
-    """Compute similarity matrix between valid and known signals."""
-    similarities = np.zeros((N_valid, N_known), dtype=int)
+    # {song_id: {time_offset_diff: count}}
+    offset_matches = defaultdict(lambda: defaultdict(int))
+    
+    for query_hash_value, query_time in query_hashes:
+        if query_hash_value in fingerprint_db:
+            for db_time, song_id in fingerprint_db[query_hash_value]:
+                
+                #calculate time diff between db hash and query hash
+                time_diff = db_time - query_time
+                
+                # Round time difference to nearest decisecond for robustness
+                time_diff_rounded = round(time_diff / 10) * 10
 
-    for valid_id in tqdm(range(N_valid) , desc="Computing similarities", unit="validation signal"):
-        recognized_song = recognize_signal(valid_signals[valid_id], fingerprint_db)
+                #Count the occurrence of this time offset for particular song
+                offset_matches[song_id][time_diff_rounded] += 1
+    
+    # For each song, find the most common time offset
+    best_matches = {}
+    for song_id, offsets in offset_matches.items():
+        # Most common offset = likely position of query in the song
+        best_offset = max(offsets, key=offsets.get)  #get offset with max count 
+        match_count = offsets[best_offset]
+        best_matches[song_id] = (match_count, best_offset)
+    
+    return best_matches
+
+def compute_similarity_matrix(signals, fingerprint_db , N_signals: int, N_known: int):
+    """Compute similarity matrix between valid and known signals."""
+    similarities = np.zeros((N_signals, N_known), dtype=int)
+
+    # for each validation/test signal, create scores and fill the similarity matrix
+    for signal_id in tqdm(range(N_signals) , desc="Computing similarities", unit="validation signal"):
+        recognized_song = recognize_signal(signals[signal_id], fingerprint_db)
         for song_index, match_count in recognized_song.items():
-            similarities[valid_id, song_index] = match_count
+            similarities[signal_id, song_index] = match_count
 
     return similarities
 
+def compute_similarity_matrix_time_offsets(signals, fingerprint_db , N_signals: int, N_known: int):
+    """Compute similarity matrix between valid and known signals."""
+    similarities = np.zeros((N_signals, N_known), dtype=int)
+    time_offsets = np.zeros((N_signals, N_known), dtype=int)
+
+    # for each validation/test signal, create scores and fill the similarity matrix
+    for signal_id in tqdm(range(N_signals) , desc="Computing similarities", unit="validation signal"):
+        recognized_songs = recognize_signal_time_offset(signals[signal_id], fingerprint_db)
+
+        for song_id, (match_count, time_offset) in recognized_songs.items():
+            similarities[signal_id, song_id] = match_count
+            time_offsets[signal_id, song_id] = time_offset 
+    return similarities, time_offsets
+
+
+
+
 def eval(scores, key):
-  indices = np.flip(np.argsort(scores), axis=-1) # we want highest to lowest ...
-  #print(scores[0,key[0]], key[0], indices)
-  top1acc = np.sum(key == indices[:,0]) / indices.shape[0]
-  top5acc = 0
-  for ii in range(5):
-    top5acc += np.sum(key == indices[:,ii])
-  top5acc /=  indices.shape[0]
-  return top1acc, top5acc
+    indices = np.flip(np.argsort(scores), axis=-1) # we want highest to lowest ...
+    #print(scores[0,key[0]], key[0], indices)
+    top1acc = np.sum(key == indices[:,0]) / indices.shape[0]
+    top5acc = 0
+    for ii in range(5):
+        top5acc += np.sum(key == indices[:,ii])
+    top5acc /=  indices.shape[0]
+    return top1acc, top5acc
 
 
 if __name__ == "__main__":
@@ -391,8 +435,7 @@ if __name__ == "__main__":
     # print(f"Fingerprint generation took {elapsed_time:.4f} seconds")
 
     # PHASE4
-    fingerprint_db = build_fingerprint_database(known_signals, use_file=USE_FILE, filename=FILE_NAME)
-    print(f"Fingerprint database contains {len(fingerprint_db)} unique hashes.")
+    # fingerprint_db = build_fingerprint_database(known_signals, load_from_file=LOAD_FROM_FILE, save_to_file=SAVE_TO_FILE, filename=FILE_NAME)
     # for hash_value in list(fingerprint_db.keys())[:5]:
     #     print(f"Hash: {hash_value}, Entries: {fingerprint_db[hash_value]}")
 
@@ -406,43 +449,14 @@ if __name__ == "__main__":
     # recognized_song = recognize_signal(valid_signals[valid_id], fingerprint_db)
 
     # PHASE 6
-    # start_time = time.time()
-    # scores_matrix = compute_similarity_matrix(valid_signals, fingerprint_db , N_valid, N_known)
-    # end_time = time.time()
-    # print(f"Similarity matrix computed in {end_time - start_time:.2f} seconds")
-    # print("Similarity matrix shape:", scores_matrix.shape)
-    # #
-    # top1, top5 = eval(scores_matrix, key)
-    # print("Top 1 accuracy ", top1 * 100, "%, Top 5 accuracy ", top5 * 100, "%" )
-
-    # # Najdi predikce pro každou validační píseň
-    # predictions = np.argmax(scores_matrix, axis=1)  # Index nejvyšší hodnoty v každém řádku
-    # match_counts = np.max(scores_matrix, axis=1)    # Nejvyšší počet shod
-
-    # # Porovnej s klíčem
-    # correct_mask = (predictions == key)
-    # correct_predictions = predictions[correct_mask]
-
-    # print(f"Correct predictions: {len(correct_predictions)} out of {N_valid}")
-    # for i in range(N_valid):
-    #     print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {key[i]}, Matches = {match_counts[i]}")
-    # incorrect_predictions = predictions[~correct_mask]
-    # print(f"Incorrect predictions: {len(incorrect_predictions)} out of {N_valid}")
-    # for i in range(N_valid):
-    #     if predictions[i] != key[i]:
-    #         print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {key[i]}, Matches = {match_counts[i]}")
-
-        
-    # PHASE 7 test data
-
-    pred_key = np.loadtxt("predictions_key.txt", delimiter = ',', usecols=(1), dtype ='int')
+    fingerprint_db = build_fingerprint_database(known_signals, load_from_file=LOAD_FROM_FILE, save_to_file=SAVE_TO_FILE, filename=FILE_NAME)
     start_time = time.time()
-    scores_matrix = compute_similarity_matrix(test_signals, fingerprint_db , N_test, N_known)
+    scores_matrix = compute_similarity_matrix(valid_signals, fingerprint_db , N_valid, N_known)
     end_time = time.time()
     print(f"Similarity matrix computed in {end_time - start_time:.2f} seconds")
     print("Similarity matrix shape:", scores_matrix.shape)
-
-    top1, top5 = eval(scores_matrix, pred_key)
+    #
+    top1, top5 = eval(scores_matrix, key)
     print("Top 1 accuracy ", top1 * 100, "%, Top 5 accuracy ", top5 * 100, "%" )
 
     # Najdi predikce pro každou validační píseň
@@ -450,22 +464,60 @@ if __name__ == "__main__":
     match_counts = np.max(scores_matrix, axis=1)    # Nejvyšší počet shod
 
     # Porovnej s klíčem
-    correct_mask = (predictions == pred_key)
+    correct_mask = (predictions == key)
     correct_predictions = predictions[correct_mask]
 
+    print(f"Correct predictions: {len(correct_predictions)} out of {N_valid}")
+    for i in range(N_valid):
+        print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {key[i]}, Matches = {match_counts[i]}")
+    incorrect_predictions = predictions[~correct_mask]
+    print(f"Incorrect predictions: {len(incorrect_predictions)} out of {N_valid}")
+    for i in range(N_valid):
+        if predictions[i] != key[i]:
+            print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {key[i]}, Matches = {match_counts[i]}")
+
+        
+    # PHASE 7 test data
+    # fingerprint_db = build_fingerprint_database(known_signals, load_from_file=LOAD_FROM_FILE, save_to_file=SAVE_TO_FILE, filename=FILE_NAME)
+    # pred_key = np.loadtxt("predictions_key.txt", delimiter = ',', usecols=(1), dtype ='int')
+    # start_time = time.time()
+    # scores_matrix = compute_similarity_matrix(test_signals, fingerprint_db , N_test, N_known)
+    # end_time = time.time()
+    # print(f"Similarity matrix computed in {end_time - start_time:.2f} seconds")
+    # print("Similarity matrix shape:", scores_matrix.shape)
+
+    # top1, top5 = eval(scores_matrix, pred_key)
+    # print("Top 1 accuracy ", top1 * 100, "%, Top 5 accuracy ", top5 * 100, "%" )
+
+    # # Najdi predikce pro každou validační píseň
+    # predictions = np.argmax(scores_matrix, axis=1)  # Index nejvyšší hodnoty v každém řádku
+    # match_counts = np.max(scores_matrix, axis=1)
+    
+    # # for each prediction get time offset
+    # # predicted_time_offsets = time_offsets[np.arange(N_test), predictions]
 
 
-    # Porovnej s klíčem
-    # correct_mask = (predictions == key)
+    # # Porovnej s klíčem
+    # correct_mask = (predictions == pred_key)
     # correct_predictions = predictions[correct_mask]
 
     # print(f"Correct predictions: {len(correct_predictions)} out of {N_valid}")
     # for i in range(N_valid):
-    #     print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {key[i]}, Matches = {match_counts[i]}")
+    #     print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {pred_key[i]}, Matches = {match_counts[i]}")
     # incorrect_predictions = predictions[~correct_mask]
     # print(f"Incorrect predictions: {len(incorrect_predictions)} out of {N_valid}")
     # for i in range(N_valid):
-    #     if predictions[i] != key[i]:
-    #         print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {key[i]}, Matches = {match_counts[i]}")
+    #     if predictions[i] != pred_key[i]:
+    #         print(f"Validation song {i}: Predicted = {predictions[i]}, Actual = {pred_key[i]}, Matches = {match_counts[i]}")
+
+    
+    # song_id, matches, offset = recognize_signal_time_offset(valid_signals[15], fingerprint_db)
+    # print(f"Matched song: {song_id}")
+    # print(f"Matches: {matches}")
+    # print(f"Query starts at position: {offset/100:.1f}s in the known song")
+
+    # PHASE 8 test data with time offset
+    # fingerprint_db = build_fingerprint_database(known_signals, load_from_file=LOAD_FROM_FILE, save_to_file=SAVE_TO_FILE, filename=FILE_NAME)
+    # start_time = time.time()
 
 
